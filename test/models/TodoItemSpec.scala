9201130207fa005610d2078cd323ed9e6e0fbf74
Jose Saldana <jose.saldana@admios-sa.com>
2015-03-16 16:56:19 -0500

    Added spec for TodoItem model

--- test/models/TodoItemSpec.scala
+++ test/models/TodoItemSpec.scala
@@ -1,8 +1,27 @@
 package models
 
-/**
- * Created by josesaldana on 3/16/15.
- */
-object TodoItemSpec {
+import play.api.test.PlaySpecification
+
+object TodoItemSpec extends PlaySpecification {
+
+  "TodoItem" should {
+    "describe a description of task and the completion status" in {
+      val todo = TodoItem("abcdefg", "Wash dishes", true)
+
+      todo must not be null
+      todo.completed must beTrue
+      todo.description must be equalTo "Wash dishes"
+    }
+
+    "provide a way to update the description of the task and/or the completion status" in {
+      val todo = TodoItem("abcdefg", "Wash dishes", true)
+
+      val updatedTodo = todo.update(Some("Clean room"), Some("true"))
+
+      updatedTodo must not be null
+      updatedTodo.description must be equalTo "Clean room"
+      updatedTodo.completed must beTrue
+    }
+  }
 
 }
